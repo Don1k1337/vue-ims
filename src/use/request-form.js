6a0c7ae39bdcc07fd5e43921f0762f08a2bffbf8
Don1k1337 <dan4ik1411@gmail.com>
2022-12-26 12:38:48 +0600

    request-form.js UI validation fixed

--- src/use/request-form.js
+++ src/use/request-form.js
@@ -15,11 +15,18 @@ export function useRequestForm(fn) {
             .trim()
             .required('Provide the name of client')
     )
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
     const {value: phone, errorMessage: ePhone, handleBlur: phoneBlur} = useField(
         'phone',
         yup.string()
+            .nullable()
+            .test('is-number', 'Phone must be a number', value => !isNaN(value))
             .trim()
             .required('Phone number cannot be empty')
+            .matches(phoneRegExp, 'Phone number does not matched the format,' +
+                ' please provide numbers only, length should be greater than 5.' +
+                ' Phone should start from numbers, do not use + sign'
+            )
     )
     const {value: amount, errorMessage: eAmount, handleBlur: amountBlur} = useField(
         'amount',
