bb995d4b3f558ebe8988acaddf652762478d7a0f
Don1k1337 <dan4ik1411@gmail.com>
2023-01-03 16:10:12 +0600

    Project updates

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="layout + '-layout'" v-if="layout" />
+</template>
+
+<script>
+import {computed} from "vue";
+import {useRoute} from "vue-router";
+import AuthLayout from "./layout/AuthLayout.vue";
+import MainLayout from "./layout/MainLayout.vue";
+  export default {
+    setup() {
+      const route = useRoute()
+      return {
+        layout: computed(() => route.meta.layout)
+      }
+    },
+    components: {MainLayout, AuthLayout}
+
+  }
+</script>
